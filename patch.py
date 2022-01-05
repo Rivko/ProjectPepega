156e4baf6c49ebc3be1d272a70b40f2197e52e4c
Sergey Rivov <rvngtn@live.com>
2022-01-05 11:46:32 +0200

    changelog out

--- patch.py
+++ patch.py
@@ -1,12 +1,13 @@
+from io import StringIO
 import pandas as pd
 from loguru import logger
 import sys
 import config
 import spreadsheet
-import numpy as np
+import mmap
 
 logger.add(sys.stdout, format="{time} {level} {message}", level="DEBUG")
-# logger.add("patch_{time}.log", level="DEBUG")
+logger.add("patch.log", level="DEBUG", rotation="1 MB")
 logger.debug(
     f"Config: {config.SPREADSHEET_URL=}, {config.SHEET_NAME=}, {config.LIB_TO_PATCH=}"
 )
@@ -23,35 +24,66 @@ try:
 except ValueError as e:
     logger.error(e)
     logger.error(
-        "Couldn't parse spreadsheet. Make sure that url and column names are correct and spreadsheet is open to public."
+        "Couldn't parse the spreadsheet. Make sure that url and column names are correct and spreadsheet is open to public."
     )
     exit()
-logger.success(f"Parsed {full_url}, num of values = {len(tablichka.index)}")
+logger.success(f"Parsed {config.SHEET_NAME}, num of values = {len(tablichka.index)}")
 warning_columns = [
     "Name",
     "Type",
-    "Value hex original",
     "Disassemble",
     "Extracted Value",
 ]
 empty_warning_columns = tablichka[warning_columns].isnull().values.any()
 if empty_warning_columns:
     logger.warning(
-        f"Found empty spreadsheet values in {warning_columns} columns. Script can still work but you should probably fill these."
+        f"Found empty spreadsheet values in {warning_columns} columns. Script might still work but you should probably fill these."
     )
     logger.info(f"Empty names will be replaced with UNKNOWN")
     tablichka["Name"] = tablichka["Name"].fillna("UNKNOWN")
 
-error_columns = ["Address", "Value Override Converted to HEX"]
+error_columns = ["Address", "Value Override Converted to HEX", "Value hex original"]
 empty_error_columns = tablichka[error_columns].isnull().values.sum()
 if empty_error_columns:
-    empty_rows = [row for row in tablichka[tablichka["Address"].isna()].index]
+    empty_rows = [
+        row for row in tablichka[tablichka["Address"].isna()].index
+    ]  # TODO: fix
     empty_rows += [
         row
         for row in tablichka[tablichka["Value Override Converted to HEX"].isna()].index
+    ]
+    empty_rows += [
+        row for row in tablichka[tablichka["Value hex original"].isna()].index
     ]
     empty_rows.sort()
     logger.error(
         f"Found {empty_error_columns} empty spreadsheet value(s) with ID {empty_rows} in {error_columns} columns. Exiting..."
     )
     exit()
+
+
+try:
+    with open(config.LIB_TO_PATCH, "r+b") as lib_file:
+        lib = mmap.mmap(lib_file.fileno(), 0, access=mmap.ACCESS_WRITE)
+except FileNotFoundError as e:
+    logger.error(e)
+    exit()
+
+logger.success(f"Loaded lib file {config.LIB_TO_PATCH}")
+changed_values = tablichka.loc[
+    tablichka["Value Override Converted to HEX"] != tablichka["Value hex original"]
+]  # выбираются значения где дефолт хекс и новый хекс не совпадают
+values_to_patch = list(
+    zip(changed_values["Address"], changed_values["Value Override Converted to HEX"])
+)
+logger.info(f"Patching {len(values_to_patch)} values")
+changelog = StringIO()
+changelog.write("This is an auto-generated changelog by PepegaPatcher\n\n")
+changelog.write(
+    changed_values.to_string(
+        index=False, na_rep="", columns=["Name", "Extracted Value", "Value override"]
+    )
+)
+
+with open(config.LIB_TO_PATCH + ".txt", "w", encoding="utf-8") as f:
+    print(changelog.getvalue(), file=f)
