2423a12bc427fb1af409e06ea971637fe2a33b1e
Sergey Rivov <rvngtn@live.com>
2022-01-05 13:57:42 +0200

    patching works

--- patch.py
+++ patch.py
@@ -1,11 +1,16 @@
 from io import StringIO
+import logging
 import pandas as pd
 from loguru import logger
 import sys
 import config
 import spreadsheet
 import mmap
+from datetime import datetime
+from shutil import copyfile
 
+now = datetime.now()
+dt_string = now.strftime("[%d.%m.%Y %H:%M:%S]")
 logger.add(sys.stdout, format="{time} {level} {message}", level="DEBUG")
 logger.add("patch.log", level="DEBUG", rotation="1 MB")
 logger.debug(
@@ -20,6 +25,7 @@ except IndexError:
 
 logger.debug(f"Extracted {sheet_id=}, {full_url=}")
 try:
+    logger.info(f"Trying to access {full_url}")
     tablichka = pd.read_csv(full_url, index_col=0, usecols=config.COLUMN_NAMES)
 except ValueError as e:
     logger.error(e)
@@ -27,7 +33,9 @@ except ValueError as e:
         "Couldn't parse the spreadsheet. Make sure that url and column names are correct and spreadsheet is open to public."
     )
     exit()
-logger.success(f"Parsed {config.SHEET_NAME}, num of values = {len(tablichka.index)}")
+logger.success(
+    f"Parsed {config.SHEET_NAME}, total num of values = {len(tablichka.index)}"
+)
 warning_columns = [
     "Name",
     "Type",
@@ -45,45 +53,65 @@ if empty_warning_columns:
 error_columns = ["Address", "Value Override Converted to HEX", "Value hex original"]
 empty_error_columns = tablichka[error_columns].isnull().values.sum()
 if empty_error_columns:
-    empty_rows = [
-        row for row in tablichka[tablichka["Address"].isna()].index
-    ]  # TODO: fix
-    empty_rows += [
-        row
-        for row in tablichka[tablichka["Value Override Converted to HEX"].isna()].index
-    ]
-    empty_rows += [
-        row for row in tablichka[tablichka["Value hex original"].isna()].index
-    ]
+    empty_rows = []
+    for column in error_columns:
+        empty_rows += [
+            row for row in tablichka[tablichka[column].isna()].index
+        ]  # список индексов с пустыми строками
     empty_rows.sort()
     logger.error(
         f"Found {empty_error_columns} empty spreadsheet value(s) with ID {empty_rows} in {error_columns} columns. Exiting..."
     )
     exit()
 
+try:
+    NEW_LIB_NAME = config.LIB_TO_PATCH.split(".so")[0] + "_" + dt_string
+    logger.debug(f"Copying {config.LIB_TO_PATCH} to /patched/{NEW_LIB_NAME}.so")
+    copyfile(config.LIB_TO_PATCH, "patched/" + NEW_LIB_NAME + ".so")
+except Exception as e:
+    logger.error(e)
+    exit()
 
 try:
-    with open(config.LIB_TO_PATCH, "r+b") as lib_file:
+    with open("patched/" + NEW_LIB_NAME + ".so", "r+b") as lib_file:
+        logger.debug(f"Trying to open {NEW_LIB_NAME} in /patched")
         lib = mmap.mmap(lib_file.fileno(), 0, access=mmap.ACCESS_WRITE)
 except FileNotFoundError as e:
     logger.error(e)
     exit()
 
-logger.success(f"Loaded lib file {config.LIB_TO_PATCH}")
+logger.success(f"Loaded lib file {NEW_LIB_NAME}.so")
 changed_values = tablichka.loc[
     tablichka["Value Override Converted to HEX"] != tablichka["Value hex original"]
 ]  # выбираются значения где дефолт хекс и новый хекс не совпадают
 values_to_patch = list(
     zip(changed_values["Address"], changed_values["Value Override Converted to HEX"])
 )
+if changed_values["Value Override Converted to HEX"].str.contains("Loading").any():
+    logger.error(
+        "Google Spreadsheet is still processing some values. Please wait a minute and try running the script again."
+    )
+    exit()
+
 logger.info(f"Patching {len(values_to_patch)} values")
+for value in values_to_patch:
+    logger.debug(f"Patching address = {value[0]}, value = {value[1]}")
+    try:
+        lib.seek(int(value[0], 16), 0)
+        lib.write(bytes.fromhex(value[1]))
+    except ValueError as e:
+        logger.error(f"Address {value[0]} is out of range.")
+        exit()
+lib.flush()
+
 changelog = StringIO()
-changelog.write("This is an auto-generated changelog by PepegaPatcher\n\n")
+changelog.write(
+    f"This is an auto-generated changelog by PepegaPatcher\nPatched on {dt_string}\n"
+)
 changelog.write(
     changed_values.to_string(
         index=False, na_rep="", columns=["Name", "Extracted Value", "Value override"]
     )
 )
-
-with open(config.LIB_TO_PATCH + ".txt", "w", encoding="utf-8") as f:
+with open("patched/" + NEW_LIB_NAME + ".txt", "w", encoding="utf-8") as f:
     print(changelog.getvalue(), file=f)
