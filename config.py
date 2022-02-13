c09fb4a573c1d7103f9c86431e09d14855c1bc6f
Sergey Rivov <rvngtn@live.com>
2022-02-13 08:43:22 +0200

    fix

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ HBK_NAME = "8.4.300_rc15.hbk"
 LIB_TO_EXTRACT = "8.4.400_rc19.so"
 
 
-SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1sb-zpSb62dLRMYv61_WcSoZrclXZJUIhrUilkoU1mbU/edit#gid=2135840244"  # MUST BE OPEN TO PUBLIC
+SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1sb-zpSb62dLRMYv61_WcSoZrclXZJUIhrUilkoU1mbU"  # MUST BE OPEN TO PUBLIC
 SHEET_NAME = "8.4"  # NO SPACES
 LIB_TO_PATCH = "8.4.400_rc19.so"
 
